f8f10786dd5cbb672b7c5ac39932089870fe7e18
Sanagupta29 <sanagupta29@gmail.com>
2022-12-01 21:36:18 +0530

    frontend

---
+++ src/components/About.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import './About.css'
+
+const About = () => {
+    return (
+        <div className="section">
+        <div className="container">
+            <div className="content-section">
+                <div className="title">
+                    <h1>About Us</h1>
+                </div>
+                <div className="content">
+                    <h3>Why a writer's club is important?</h3>
+                    <p>
+                    1) Knowledge:
+The writer’s group can give you new ideas for stories, provide feedback on your work and help with writer’s block. You will learn about different aspects of writing that are useful to know as a writer. If there is one writer in the group who has written something similar to what you want to write, they may be able to help you with writer’s block. By being part of a writer’s group, you can also learn things that are useful in other aspects of life.
+<br></br>
+2) Community:
+A writer needs support just like any artist does! A writer is constantly putting themselves out there by sharing their work and letting others read it; having people who understand your struggles and support you is so important. There are also writers who have been published or have had their work read by an audience, which can be a great inspiration to writers seeking publication as well!
+<br></br>
+3) Accountability:
+A writer needs accountability just like any other professional does in order to stay on track with meeting deadlines and following through with projects. A writer’s group can hold each other accountable for writing on a regular basis. It is so easy to be distracted by work, family and life in general that having the support of a writer’s group will help you stay focused.
+<br></br>
+4) Feedback:
+Most writers are not perfect; they need feedback from others along the way! Having people willing to read your work and provide feedback is such a benefit. Feedback can be very encouraging for writers who may not receive much constructive criticism on their writing otherwise.
+<br></br>
+5) Motivation:
+There are times when writers get discouraged or lose motivation to keep working at their craft; having the support of a writer’s group will help you stay motivated! When writer’s feel discouraged or lose motivation, the writer’s group can help them get back on track.
+<br></br>
+6) Exposure:
+Writer’s groups are great places to meet other writers and find inspiration! Meeting other writers in your area who share similar interests is so beneficial. You never know when you may learn something new about the craft of writing that will inspire you or help you in your own work.
+<br></br>
+7) Creativity:
+Sometimes writer’s block can be the result of writers not feeling creative! The writers’ group is a great place to share ideas and learn new ways to approach writing challenges. Having a writers group will help spark creativity within writers when they are stumped or feel like their work has gone stale.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    )
+  }
+  
+  export default About
---
+++ src/components/BData.js
@@ -0,0 +1,28 @@
+const data = [
+    
+    {   id : 1,
+        title  : "November 9",
+        content : "Fallon meets Ben, an aspiring novelist, the day before her scheduled cross-country move. Their untimely attraction leads them to spend Fallon’s last day in L.A. together, and her eventful life becomes the creative inspiration Ben has always sought for his novel. Over time and amidst the various relationships and tribulations of their own separate lives, they continue to meet on the same date every year. Until one day Fallon becomes unsure if Ben has been telling her the truth or fabricating a perfect reality for the sake of the ultimate plot twist. Can Ben’s relationship with Fallon—and simultaneously his novel—be considered a love story if it ends in heartbreak? Beloved #1 New York Times bestselling author Colleen Hoover returns with an unforgettable love story between a writer and his unexpected muse.",
+        author  : "Colleen Hoover"
+
+    },
+    {   id : 2,
+        title  : "And Then There Were None",
+        content : "First, there were ten—a curious assortment of strangers summoned as weekend guests to a little private island off the coast of Devon. Their host, an eccentric millionaire unknown to all of them, is nowhere to be found. All that the guests have in common is a wicked past they're unwilling to reveal—and a secret that will seal their fate. For each has been marked for murder. A famous nursery rhyme is framed and hung in every room of the mansion. When they realize that murders are occurring as described in the rhyme, terror mounts. One by one they fall prey. Before the weekend is out, there will be none. Who has choreographed this dastardly scheme? And who will be left to tell the tale? Only the dead are above suspicion.",
+        author  : "Agatha Christie"
+
+    },
+    {   id : 3,
+        title  : "It Ends with Us",
+        content : "Sometimes it is the one who loves you who hurts you the most. Lily hasn’t always had it easy, but that’s never stopped her from working hard for the life she wants. She’s come a long way from the small town in Maine where she grew up — she graduated from college, moved to Boston, and started her own business. So when she feels a spark with a gorgeous neurosurgeon named Ryle Kincaid, everything in Lily’s life suddenly seems almost too good to be true. Ryle is assertive, stubborn, maybe even a little arrogant. He’s also sensitive, brilliant, and has a total soft spot for Lily. And the way he looks in scrubs certainly doesn’t hurt. Lily can’t get him out of her head. But Ryle’s complete aversion to relationships is disturbing. Even as Lily finds herself becoming the exception to his “no dating” rule, she can’t help but wonder what made him that way in the first place. As questions about her new relationship overwhelm her, so do thoughts of Atlas Corrigan — her first love and a link to the past she left behind. He was her kindred spirit, her protector. When Atlas suddenly reappears, everything Lily has built with Ryle is threatened.",
+        author  : "Collen Hoover"
+
+    },
+    {   id : 4,
+        title  : "The Hating Game",
+        content : "Lucy Hutton has always been certain that the nice girl can get the corner office. She’s charming and accommodating and prides herself on being loved by everyone at Bexley & Gamin. Everyone except for coldly efficient, impeccably attired, physically intimidating Joshua Templeman. And the feeling is mutual. Trapped in a shared office together 40 (OK, 50 or 60) hours a week, they’ve become entrenched in an addictive, ridiculous never-ending game of one-upmanship. There’s the Staring Game. The Mirror Game. The HR Game. Lucy can’t let Joshua beat her at anything—especially when a huge new promotion goes up for the taking. If Lucy wins this game, she’ll be Joshua’s boss. If she loses, she’ll resign. So why is she suddenly having steamy dreams about Joshua, and dressing for work like she’s got a hot date? After a perfectly innocent elevator ride ends with an earth shattering kiss, Lucy starts to wonder whether she’s got Joshua Templeman all wrong. Maybe Lucy Hutton doesn’t hate Joshua Templeman. And maybe, he doesn’t hate her either. Or maybe this is just another game.",
+        author  : "Sally Thorne"
+
+    },
+]
+export default data;
